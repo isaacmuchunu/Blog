b85c4f9818ef5f0b3630e9b33955422d0aaf8df0
isaac <isaacmuchunu@gmail.com>
2023-09-13 14:11:51 +0300

    changed the pading on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ const MoreItems = [
 const Footer = () => {
   return (
     <section className="bg-dark-hard">
-      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10">
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-10 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10">
         <div className="col-span-5 md:col-span-4 lg:col-span-2">
           <h3 className="text-dark-light font-bold md:text-lg font-poppins">
             Products
